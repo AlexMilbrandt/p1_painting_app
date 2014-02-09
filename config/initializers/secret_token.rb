9b30e073f1834a16b0f606c7af36bf065bc6cf0e
Alex Milbrandt <amilbrandt@wisc.edu>
2014-02-08 20:53:59 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+P1PaintingApp::Application.config.secret_key_base = 'ad5435a2d7f5dbfe1d136d0b22efaea887f7f8ea2d12e726f9ab869ba6635d575a7ba30d7a5192758697e6e746d75125ee1363bca1d0d4d3b548c17398b86f97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+P1PaintingApp::Application.config.session_store :cookie_store, key: '_p1_painting_app_session'
